57c2eccd559edc0c175b46e9744b0c7ca141bce7
DavidMEa <mea.david29@outlook.com>
2025-06-25 21:10:52 +0400

    starting chapter 3

---
+++ questions/chapter_3.js
@@ -0,0 +1,9 @@
+//if you add a string with an integer it gives you a string 
+let a = '5' + 3
+let b = a -3 // it becomes a number when you do that 
+console.log(typeof b)
+
+//arrays 
+
+let anArray = [[0], [9], [3]]
+ console.log(anArray[0][0])
